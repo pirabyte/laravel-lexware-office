af4d57b1761549858eb3e56320d6c5974e1e6f8d
Felix D. <24978665+felix-exon@users.noreply.github.com>
2025-04-10 20:25:35 +0200

    posting category tests

--- tests/Feature/FinancialTransactionResourceTest.php
+++ tests/Feature/FinancialTransactionResourceTest.php
@@ -2,11 +2,55 @@
 
 namespace Pirabyte\LaravelLexwareOffice\Tests\Feature;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use OutOfRangeException;
+use Pirabyte\LaravelLexwareOffice\Facades\LexwareOffice;
 use Pirabyte\LaravelLexwareOffice\Models\FinancialTransaction;
 use Pirabyte\LaravelLexwareOffice\Tests\TestCase;
 
 class FinancialTransactionResourceTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Mock-Responses für die API-Aufrufe bei Personen-Kontakt
+        $personMockResponses = [
+            // Response für create
+            new Response(201, ['Content-Type' => 'application/json'], json_encode([
+                'id' => '123e4567-e89b-12d3-a456-426614174000',
+                'resourceUri' => 'https://api.lexoffice.io/v1/contacts/123e4567-e89b-12d3-a456-426614174000',
+                'createdDate' => '2023-06-29T15:15:09.447+02:00',
+                'updatedDate' => '2023-06-29T15:15:09.447+02:00',
+                'version' => 1
+            ])),
+            // Response für get (innerhalb von create)
+            new Response(200, ['Content-Type' => 'application/json'], json_encode([
+                'id' => '123e4567-e89b-12d3-a456-426614174000',
+                'version' => 1,
+                'roles' => [
+                    'customer' => ['number' => 'K-12345']
+                ],
+                'person' => [
+                    'salutation' => 'Herr',
+                    'firstName' => 'Max',
+                    'lastName' => 'Mustermann'
+                ]
+            ])),
+        ];
+
+        $mock = new MockHandler($personMockResponses);
+        $handlerStack = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handlerStack]);
+
+        // Client mit Mock-Handler ersetzen
+        $instance = app('lexware-office');
+        $instance->setClient($client);
+    }
+
     public function test_it_can_create_valid_financial_transaction_json()
     {
         $fixtureFile = __DIR__ . '/../Fixtures/financial-transactions/1_financial_transactions_response.json';
@@ -41,6 +85,17 @@ class FinancialTransactionResourceTest extends TestCase
             $financialTransaction = FinancialTransaction::fromArray($fixtureFinancialTransaction);
             $this->validate_financial_transaction_with_fixture_data($financialTransaction, $fixtureFinancialTransaction);
         }
+    }
+
+    public function test_creating_more_than_25_transactions_throws_an_exception()
+    {
+        $transactions = [];
+        for($i = 1; $i <= 26; ++$i) {
+            $transactions[] = new FinancialTransaction();
+        }
+        $this->assertThrows(function () use ($transactions) {
+            LexwareOffice::financialTransactions()->create($transactions);
+        }, OutOfRangeException::class);
     }
 
     private function validate_financial_transaction_with_fixture_data(FinancialTransaction $transaction, array $fixtureData): void
--- tests/Feature/PostingCategoryResourceTest.php
+++ tests/Feature/PostingCategoryResourceTest.php
@@ -2,11 +2,48 @@
 
 namespace Pirabyte\LaravelLexwareOffice\Tests\Feature;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use Pirabyte\LaravelLexwareOffice\Enums\PostingCategoryType;
+use Pirabyte\LaravelLexwareOffice\Facades\LexwareOffice;
 use Pirabyte\LaravelLexwareOffice\Models\PostingCategory;
 use Pirabyte\LaravelLexwareOffice\Tests\TestCase;
 
 class PostingCategoryResourceTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $fixtureFile = __DIR__ . '/../Fixtures/posting-categories/1_parse_posting_category_response.json';
+        $fixtureData = file_get_contents($fixtureFile);
+
+        // Mock-Responses für die API-Aufrufe bei Personen-Kontakt
+        $personMockResponses = [
+            // Response für create
+            new Response(200, ['Content-Type' => 'application/json'], $fixtureData),
+            new Response(200, ['Content-Type' => 'application/json'], $fixtureData),
+        ];
+
+        $mock = new MockHandler($personMockResponses);
+        $handlerStack = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handlerStack]);
+
+        // Client mit Mock-Handler ersetzen
+        $instance = app('lexware-office');
+        $instance->setClient($client);
+    }
+
+    public function test_it_can_get_posting_categories_by_type()
+    {
+        $categories = LexwareOffice::postingCategories()->get();
+        $this->assertCount(4, $categories);
+
+        $categories = LexwareOffice::postingCategories()->get(PostingCategoryType::EXPENSE);
+        $this->assertCount(2, $categories);
+    }
 
     public function test_it_can_parse_posting_category_response()
     {
@@ -19,7 +56,7 @@ class PostingCategoryResourceTest extends TestCase
         }
     }
 
-    private function validate_posting_category(PostingCategory $postingCategory, array $fixtureData)
+    private function validate_posting_category(PostingCategory $postingCategory, array $fixtureData): void
     {
         $this->assertInstanceOf(PostingCategory::class, $postingCategory);
 
